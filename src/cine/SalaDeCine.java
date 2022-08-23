398dde033e7396e387bb8b54dc9ff3a6fc7a3eff
mora avalos <moavalos@alumno.unlam.edu.ar>
2022-08-23 15:23:25 -0300

    falta poko

--- src/cine/SalaDeCine.java
+++ src/cine/SalaDeCine.java
@@ -8,8 +8,8 @@ public class SalaDeCine {
 
 	public SalaDeCine() {
 		/*
-		 * this.butacas = new Integer[210];
-		 * this.filas = new Integer[15]; // en cada fila entran 14 butacas
+		 * this.butacas = new Integer[210]; this.filas = new Integer[15]; // en cada
+		 * fila entran 14 butacas
 		 */
 		this.columnas = 14; // 210 butacas
 		this.filas = 15;
@@ -17,48 +17,75 @@ public class SalaDeCine {
 	}
 
 	// 2
-	public Boolean butacaOcupada(Integer columna, Integer fila) {
+	public Boolean butacaOcupada(Integer columna, Integer fila) { // ESTA MIERDA NO ANDA
 		// esto devuelve true si la butaca esta disponible/desocupada.
 		Boolean desocupado = false;
 		for (int i = 0; i < asientos.length; i++) {
 			for (int j = 0; j < asientos[0].length; j++) {
-				if (asientos[i][j] == null) {
+				if (asientos[columna][fila] == null) {
 					desocupado = true;
 				}
 			}
 		}
 		return desocupado;
 	}
-	
+
 	public Boolean ocuparButacaPrueba(Integer columna, Integer fila) {
-		if(butacaOcupada(columna-1, fila-1)) {
-			asientos[columna-1][fila-1] = 1;
+		if (butacaOcupada(columna - 1, fila - 1)) {
+			asientos[columna - 1][fila - 1] = 1;
 			return true;
 		}
 		return false;
 	}
 
 	// 3
-	public void ocuparButaca(Integer columna, Integer fila) {
+	public Boolean ocuparButaca(Integer columna, Integer fila) { // esto no anda
 		// ocupar una butaca si no esta ocupada
-		if (!butacaOcupada(columna, fila)) {
-			for (int i = 0; i < asientos.length; i++) {
-				for (int j = 0; j < asientos[0].length; j++) { // for para saber las columnas
+		if (butacaOcupada(columna, fila)) {
+			for (int i = 1; i < asientos.length; i++) {
+				for (int j = 1; j < asientos[0].length; j++) { // for para saber las columnas
+					asientos[columna][fila]++;
+					return true;
 				}
 			}
 		}
+		return false;
 
 	}
 
 	// 4
 	public Integer cantidadTotalButacasOcupadas() {
-		return 0;
+		Integer cont = 0;
+		for (int i = 0; i < columnas; i++) {
+			for (int j = 0; j < filas; j++) {
+				if (!butacaOcupada(i, j)) {
+					cont += 1;
+				}
+			}
+		}
+		return cont;
 	}
 
 	// 5
-	public Boolean hayEspacioPara(Integer cantidadDePersonas) {
+	public Boolean hayEspacioPara(Integer cantidadDePersonas, Integer columna, Integer fila) {
 		// consultar si es posible acomodar a un grupo de x personas, en forma contigua
-		return true;
+		for (int i = 0; i < cantidadDePersonas; i++) {
+			if (cantidadTotalButacasOcupadas() < cantidadDePersonas) {
+				return true;
+			}
+		}
+		return false; // como chota hago que pregunten por una sola fila????
 	}
+
+	/*public Integer filaDisponible(Integer columna, Integer fila) {
+		Integer contador = 0;
+		for (int i = 0; i < filas; i++) {
+			if(butacaOcupada(i, fila - 1)) {
+				contador += 1;
+				
+			}
+		}
+		return contador;
+	}*/
 
 }
--- test/cineTest/SalaDeCineTest.java
+++ test/cineTest/SalaDeCineTest.java
@@ -1,10 +1,80 @@
 package cineTest;
 
+import java.util.Scanner;
+
+import cine.SalaDeCine;
+
 public class SalaDeCineTest {
 
+	//final static Integer BUTACA_OCUPADA = 1, OCUPAR_BUTACA = 2, CANTIDAD_BUTACAS_OCUPADAS = 3, HAY_ESPACIO_PARA = 4;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Bienvenido a Hoyts. Elige la opción deseada");
+		System.out.println("Por defecto, las columnas del cine son 14 y las filas 15.\nLas butacas están compuestas por numero de columna y fila.");
+		Scanner teclado = new Scanner(System.in);
+		Integer opcionDeseada;
+		SalaDeCine sala = new SalaDeCine();
+
+		do {
+			System.out.println(" ");
+			System.out.println("Menú de opciones.");
+			System.out.println("1 - Ver cual butaca esta ocupada o disponible");
+			System.out.println("2 - Ocupar una butaca");
+			System.out.println("3 - Ver la cantidad totales de butacas disponibles");
+			System.out.println("4 - Ver cuanto espacio hay para determinado grupo de personas");
+
+			opcionDeseada = teclado.nextInt();
+
+			switch (opcionDeseada) {
+			case 1:
+				butacaOcupada(sala, teclado);
+				break;
+			case 2:
+				ocuparButaca(sala, teclado);
+				break;
+			case 3:
+				cantidadButacasOcupadas();
+				break;
+			case 4:
+				hayEspacioPara();
+				break;
+			default:
+				System.err.println("error");
+				break;
+			}
+
+		} while (opcionDeseada != 4);
+	}
+
+	private static void butacaOcupada(SalaDeCine sala, Scanner teclado) {
+		System.out.println("Ingrese el numero de columna");
+		Integer nroColumna = teclado.nextInt();
+		System.out.println("Ingrese el numero de fila");
+		Integer nroFila = teclado.nextInt();
+		
+		System.out.println("La butaca " + nroColumna + nroFila + " esta disponible? " + sala.butacaOcupada(nroColumna, nroFila));
+
+	}
+
+	private static void ocuparButaca(SalaDeCine sala, Scanner teclado) {
+		System.out.println("Ingrese el numero de columna");
+		Integer nroColumna = teclado.nextInt();
+		System.out.println("Ingrese el numero de fila");
+		Integer nroFila = teclado.nextInt();
+		
+		if(sala.ocuparButacaPrueba(nroColumna, nroFila)) {
+			System.out.println("La butaca que eligió la ocupó con exito");
+		}
+
+	}
+
+	private static void cantidadButacasOcupadas() {
+
+	}
+
+	private static void hayEspacioPara() {
+		// aca pedir la cantidad de personas e informar que fila se encuentra disponible para esa cantidad de personas
 	}
 
 }
