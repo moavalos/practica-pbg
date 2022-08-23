4d4a3a37393b45f7f242e80cd64a48e2c9d5adb7
mora avalos <moavalos@alumno.unlam.edu.ar>
2022-08-23 14:12:32 -0300

    probando

--- src/cine/SalaDeCine.java
+++ src/cine/SalaDeCine.java
@@ -4,36 +4,44 @@ public class SalaDeCine {
 
 	private Integer columnas;
 	private Integer filas;
-	private Asiento asientos[][];
+	private Integer asientos[][];
 
 	public SalaDeCine() {
 		/*
-		 * this.butacas = new Integer[210]; this.filas = new Integer[15]; // en cada
-		 * fila entran 14 butacas
+		 * this.butacas = new Integer[210];
+		 * this.filas = new Integer[15]; // en cada fila entran 14 butacas
 		 */
 		this.columnas = 14; // 210 butacas
 		this.filas = 15;
-		this.asientos = new Asiento[columnas][filas];
-		ocuparButaca();
+		this.asientos = new Integer[columnas][filas];
 	}
 
 	// 2
 	public Boolean butacaOcupada(Integer columna, Integer fila) {
+		// esto devuelve true si la butaca esta disponible/desocupada.
 		Boolean desocupado = false;
 		for (int i = 0; i < asientos.length; i++) {
 			for (int j = 0; j < asientos[0].length; j++) {
-				if(asientos[i][j] == null) {
+				if (asientos[i][j] == null) {
 					desocupado = true;
 				}
 			}
 		}
 		return desocupado;
 	}
+	
+	public Boolean ocuparButacaPrueba(Integer columna, Integer fila) {
+		if(butacaOcupada(columna-1, fila-1)) {
+			asientos[columna-1][fila-1] = 1;
+			return true;
+		}
+		return false;
+	}
 
 	// 3
-	public void ocuparButaca() {
+	public void ocuparButaca(Integer columna, Integer fila) {
 		// ocupar una butaca si no esta ocupada
-		if (!butacaOcupada(columnas, filas)) {
+		if (!butacaOcupada(columna, fila)) {
 			for (int i = 0; i < asientos.length; i++) {
 				for (int j = 0; j < asientos[0].length; j++) { // for para saber las columnas
 				}
---
+++ test/cineTest/SalaDeCineTest.java
@@ -0,0 +1,10 @@
+package cineTest;
+
+public class SalaDeCineTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
